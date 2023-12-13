97e303b19f773ec9fbdb622abe05850bae7438ac
GannJobs <linksyamato@gmail.com>
2023-12-12 22:58:56 -0300

    finished

--- categorias/views.py
+++ categorias/views.py
@@ -5,8 +5,10 @@ from .models import Categorias
 from user.models import Usuario
 from loja.models import Loja
 from produtos.models import Produto
+from produtos.serializer import ProdutosSerializer
 from .serializer import CategoriasSerializer
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 # Create your views here.
 class CategoraisModelViewSet(ModelViewSet):
@@ -19,18 +21,38 @@ class CategoraisModelViewSet(ModelViewSet):
         dono = Usuario.objects.get(Pessoa=request.user)
         loja = Loja.objects.get(Dono=dono)
         categorias = Categorias.objects.filter(Loja=loja)
+
+        categorias_data = []
+        for categoria in categorias:
+            produtos = Produto.objects.filter(Categoria=categoria)
+            produtos_serializer = ProdutosSerializer(produtos, many=True)
+            categoria_data = {
+                'id': categoria.id,
+                'nome': categoria.Nome,
+                'descricao': categoria.Descricao,
+                'produtos': produtos_serializer.data,
+            }
+            categorias_data.append(categoria_data)
+        return Response({'status': 200, 'Categorias': categorias_data})
+   
+    @action(methods=["get"], detail=False)
+    def espec(self, request):  
+        id = request.data.get('id')
+        loja = Loja.objects.get(id=id)
+        categorias = Categorias.objects.filter(Loja=loja)
         serializer = CategoriasSerializer(categorias, many=True)
         return Response({'status': 200, 'Categorias': serializer.data})
-    
+
     def create(self, request):
         nome = request.data.get('nome')
         descricao = request.data.get('descricao')
-        loja = request.data.get('loja')
+        loja = request.data.get('lojaId')
         Lj = Loja.objects.get(id=loja)
         Categorias.objects.create(Nome=nome, Descricao=descricao, Loja=Lj)
         return Response({'status': 200, 'msg': 'created'})
 
-    def delete(self, request, id):
+    def delete(self, request):
+        id = request.GET.get('id')
         categoria = Categorias.objects.get(id=id)
         produtos = Produto.objects.filter(Categoria=categoria)
         for obj in produtos:
--- produtos/views.py
+++ produtos/views.py
@@ -14,15 +14,18 @@ class ProdutoModelViewSet(ModelViewSet):
     queryset = Produto.objects.all()
 
     def create(self, request):
-        categoria = request.data.get('categoria')
+        categoria = request.data.get('Catid')
         Cat = Categorias.objects.get(id=categoria)
         nome = request.data.get('nome')
         valor = request.data.get('valor')
         marca = request.data.get('marca')
-        Produto.objects.create(Nome=nome,Valor=valor,Marca=marca,Categoria=Cat)
+        img = request.data.get('img')
+        Produto.objects.create(Nome=nome,Valor=valor,img=img,Marca=marca,Categoria=Cat)
         return Response({'status': 200, 'msg': 'created'})
 
-    def delete(self, request, id):
+    def delete(self, request):
+        id = request.GET.get('id')
         prod = Produto.objects.get(id=id)
+        print(id)
         prod.delete()
         return Response({'status': 200, 'msg': 'deleted Product'})
